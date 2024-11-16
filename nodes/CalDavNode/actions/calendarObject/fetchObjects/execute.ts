460309aef1225aabe26c00d0bffa8ce9c0cf1ba0
Oleg Tsvetkov <me@olegtsvetkov.com>
2024-11-16 11:16:06 +0300

    Implement calendar object parsing

--- nodes/CalDavNode/actions/calendarObject/fetchObjects/execute.ts
+++ nodes/CalDavNode/actions/calendarObject/fetchObjects/execute.ts
@@ -1,48 +1,39 @@
 import {IExecuteFunctions, INodeExecutionData} from "n8n-workflow";
 import {createClient} from "../../../../../transport/davClient";
-import {DAVCalendar} from "tsdav";
-// import {parseCard} from "../../../../../transport/davResponseParser";
+import {DAVCalendar, DAVCalendarObject} from "tsdav";
+import { CalendarComponent, VEvent } from "../../../ical";
+
+// @ts-ignore
+import ical from "ical"
 
 export async function fetchObjects(this: IExecuteFunctions, index: number): Promise<INodeExecutionData[]> {
 	const client = await createClient(this, 'caldav')
 	const calendarObjectUrl = this.getNodeParameter('calendar', index)
 
+	// Retrieve calendars
 	const calendars: DAVCalendar[] = await client.fetchCalendars()
-
 	const calendar = calendars.find(obj => {
 		return obj.url === calendarObjectUrl
 	})
 
-	// const request = {
-	// 	url: calendarObjectUrl,
-	// 	props: [
-	// 		{
-	// 			name: 'getetag',
-	// 			namespace: DAVNamespace.DAV
-	// 		}
-	// 	],
-	// 	filters: [
-	// 		{
-	// 			'comp-filter': {
-	// 				_attributes: {
-	// 					name: 'VCALENDAR',
-	// 				},
-	// 			},
-	// 		},
-	// 	],
-	// 	depth: '1'
-	// };
-	// const response: DAVResponse[] = await client.calendarQuery(request)
-
-	const response = await client.fetchCalendarObjects({
+	// Retrieve calendar objects
+	const response: DAVCalendarObject[] = await client.fetchCalendarObjects({
 		calendar: calendar,
 		timeRange: {
-			start: '2024-11-10T00:00:00Z',
-			end: '2024-11-10T23:59:00Z'
+			start: '2024-11-16T00:00:00Z',
+			end: '2024-11-16T23:59:00Z'
 		}
 	});
 
+	// Parse to events
+	const events = response.map(r => {
+		const data = Object.values<CalendarComponent>(ical.parseICS(r.data))
+		const events = data.filter((item) => item.type === 'VEVENT') as VEvent[]
+
+		return events[0]
+	});
+
 	return this.helpers.returnJsonArray({
-		response,
+		events: events
 	})
 }
